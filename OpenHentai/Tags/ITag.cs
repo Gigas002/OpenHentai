5e5fcd26f8319b0010db77e08326576d62c998e4
gigas002 <gigas002@pm.me>
2023-04-10 21:31:37 +0900

    Attempt to re-implement a new tagging interface

--- OpenHentai/Tags/ITag.cs
+++ OpenHentai/Tags/ITag.cs
@@ -1,24 +1,89 @@
 namespace OpenHentai.Tags;
 
-/// <summary>
-/// Used for string-representated tags
-/// </summary>
+/*
+ * Vision of tagging system (at that point of time)
+
+      `tags.sql`
+
+id category value description additional_info 
+
+
+      `tags_relations.sql`
+
+master_id slave_ids
+
+
+     `tags_categories.sql` (not yet sure if we'll need it)
+
+id value
+
+
+Then, in `authors.sql`, e.g:
+
+
+...            tag_ids             ...
+...  [array of ids from `tags.sql`]  ...
+
+
+the example of corresponding values in `tags.sql` will be:
+
+
+id  cat_id    val
+0     10    "genre1"
+1     11    "clothes1"
+2     12    "genre3"
+
+
+in `tags_relations.sql`:
+
+
+m_id    s_ids
+ 0      [1, 2]
+
+
+in `tags_categories`:
+
+
+id   value
+10   genre
+11  clothes
+
+ * 
+ */
+
 // see: https://ehwiki.org/wiki/Gallery_Tagging for reference during the development
 // also: https://repo.e-hentai.org/tools.php?act=taggroup&show=8 tag repo
-// TODO: master/slave tag system
-public interface ITag
+
+/// <summary>
+/// Basic interface for tagging purposes
+/// </summary>
+public interface ITag : IDatabaseEntry
 {
+    #region Properties
+
+    // TODO: ulong and default value
+    /// <summary>
+    /// This tag's master tag
+    /// <para/>-1 in case there's no master
+    /// </summary>
+    public int MasterId { get; set; }
+    
+    /// <summary>
+    /// Collection of dependent/alternative values's ids
+    /// </summary>
+    public IEnumerable<int> SlavesIds { get; set; }
+    
+    /// <summary>
+    /// Category of this tag
+    /// e.g. `parody`
+    /// </summary>
+    public TagCategory Category { get; set; }
+    
     /// <summary>
     /// Tag's value
     /// e.g. Mitsudomoe
     /// </summary>
     public string Value { get; set; }
-    
-    /// <summary>
-    /// Alternative tag values
-    /// e.g. Three Way Struggle, みつどもえ
-    /// </summary>
-    public IEnumerable<string> AlternativeValues { get; set; }
     
     /// <summary>
     /// Tag description
@@ -32,8 +97,29 @@ public interface ITag
     /// </summary>
     public string AdditionalInfo { get; set; }
 
-    /// <summary>
-    /// Category of this tag
-    /// </summary>
-    public TagCategory Category { get; set; }
+    #endregion
+
+    #region Methods
+    
+    // Arguable if this is really needed inside interface
+    
+    // /// <summary>
+    // /// Get tag from tag source
+    // /// </summary>
+    // /// <returns>ITag object</returns>
+    // public ITag GetTag();
+    //
+    // /// <summary>
+    // /// Get master tag from tag source
+    // /// </summary>
+    // /// <returns>ITag object</returns>
+    // public ITag GetMaster();
+    //
+    // /// <summary>
+    // /// Get slave tags in case it's master tag from tag source
+    // /// </summary>
+    // /// <returns>Collection of ITag objects</returns>
+    // public IEnumerable<ITag> GetSlaves();
+
+    #endregion
 }
