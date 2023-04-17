7d9b77e79340b0fdf07d0b65511a236f2fe193c6
gigas002 <gigas002@pm.me>
2023-04-17 23:01:15 +0900

    Remove todo in creationcollection interface

--- OpenHentai/Creations/ICreationCollection.cs
+++ OpenHentai/Creations/ICreationCollection.cs
@@ -2,9 +2,8 @@ namespace OpenHentai.Creations;
 
 /// <summary>
 /// Collection of related creations, e.g. tankoubon, magazine, serialization
-/// TODO: Compelte creation collection. Should this be ICreation too?
 /// </summary>
-public interface ICreationCollection
+public interface ICreationCollection : ICreation
 {
-    
+    // Relations collection is more than enough
 }
