c13ff3bc516715c52294a39f952db4dd40bb4914
gigas002 <gigas002@pm.me>
2023-05-12 17:27:55 +0900

    Inherit Tag from ITag

--- OpenHentai.Database/Tag.cs
+++ OpenHentai.Database/Tag.cs
@@ -1,8 +1,27 @@
 using OpenHentai.Tags;
+using OpenHentai.Descriptors;
 
 namespace OpenHentai.Database;
 
+/// <inheritdoc />
 public class Tag : ITag
 {
+    #region Properties
+
+    /// <inheritdoc />
+    public ulong? MasterId { get; set; }
     
+    /// <inheritdoc />
+    public IEnumerable<ulong> SlaveIds { get; set; }
+    
+    /// <inheritdoc />
+    public TagCategory Category { get; set; }
+    
+    /// <inheritdoc />
+    public string Value { get; set; }
+    
+    /// <inheritdoc />
+    public DescriptionInfo Description { get; set; }
+
+    #endregion
 }
