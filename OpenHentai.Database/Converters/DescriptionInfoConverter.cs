d35c941d7e5306d5de4c0c9da1be7de19c3d5f85
gigas002 <gigas002@pm.me>
2023-05-31 11:32:17 +0900

    Fix namespaces in OpenHentai.Database.Converters

--- OpenHentai.Database/Converters/DescriptionInfoConverter.cs
+++ OpenHentai.Database/Converters/DescriptionInfoConverter.cs
@@ -2,7 +2,7 @@ using System.Text.Json;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OpenHentai.Descriptors;
 
-namespace OpenHentai.Database;
+namespace OpenHentai.Database.Converters;
 
 // TODO: Probably a better way will be introduced for handling json sql poperties in net8
 
