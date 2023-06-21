7e22fecab3310113d3c5ff0f6156ae4d9a7f444d
gigas002 <gigas002@pm.me>
2023-06-21 20:54:27 +0900

    Implement IDisposable and IAsyncDisposable for controllers

--- OpenHentai.WebAPI/Controllers/DatabaseController.cs
+++ OpenHentai.WebAPI/Controllers/DatabaseController.cs
@@ -1,11 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 using OpenHentai.Contexts;
 
-// TODO: disposable
-
-public abstract class DatabaseController : ControllerBase
+public abstract class DatabaseController : ControllerBase, IDisposable, IAsyncDisposable
 {
-    #region Properties
+    #region Properties/fields
+
+    protected bool IsDisposed { get; set; } = false;
 
     public DatabaseContext Context { get; init; }
 
@@ -20,6 +20,42 @@ public abstract class DatabaseController : ControllerBase
     {
         Context = context;
     }
+
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (IsDisposed) return;
+        if (disposing)
+        { }
+
+        Context.Dispose();
+
+        IsDisposed = true;
+    }
+
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public async ValueTask DisposeAsync()
+    {
+        await DisposeAsyncCore();
+
+        Dispose(false);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual async ValueTask DisposeAsyncCore()
+    {
+        if (IsDisposed) return;
+        
+        IsDisposed = true;
+    }
+
+    ~DatabaseController() => Dispose(false);
 
     #endregion
 }
