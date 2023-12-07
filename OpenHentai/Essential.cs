3ed5bbc7e0327cdc589808154dbc53f498f9a5d6
gigas002 <gigas002@pm.me>
2023-12-07 21:09:37 +0900

    Simplify json naming policy
    Remove humanizer dependency

--- OpenHentai/Essential.cs
+++ OpenHentai/Essential.cs
@@ -1,18 +1,16 @@
 using System.Text.Encodings.Web;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using OpenHentai.Helpers;
 
 namespace OpenHentai;
 
 public static class Essential
 {
-    // TODO: net8+ PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower;
     public static JsonSerializerOptions JsonSerializerOptions => new()
     {
         // allows writing of cyrillic and other symbols as-is
         Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
-        PropertyNamingPolicy = new SnakeCaseLowerNamingPolicy(),
+        PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower,
         // WriteIndented = true,
         DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault
     };
